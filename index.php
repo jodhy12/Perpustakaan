601ed1eda74b1e259878c4399658d971470b0041
nurmuhamadsoleh <nursoleh33@gmail.com>
2021-10-04 15:42:00 +0700

    deafult branch

--- index.php
+++ index.php
@@ -5,6 +5,5 @@
 </head>
 <body>
 	Ini adalah repository untuk belajar Laravel x Vue
-	Ini adalah Nur Muhamad Soleh
 </body>
 </html>
--- soleh/crud/crud_katalog/delete.php
+++
@@ -1,9 +0,0 @@
-<?php
-    include_once("../connect.php");
-    // isbn yang kita panggil dari file sebelunya
-    $id_katalog = $_GET['id_katalog'];
-
-    $result = mysqli_query($mysqli, "DELETE FROM katalog WHERE id_katalog='$id_katalog'");
-    // After Delet redirect to Home, so that latest user list will be displayed
-    header("Location:index.php");
-?>
